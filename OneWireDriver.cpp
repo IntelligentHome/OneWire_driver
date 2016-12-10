575027a9ee846fb13548a1dbb7b32439c5fe83f4
Mariusz Sikora <msikora87@gmail.com>
2016-12-10 21:47:03 +0100

    Initial project commit

---
+++ OneWireDriver.cpp
@@ -0,0 +1,7 @@
+#include "OneWireDriver.h"
+
+namespace one_wire_driver {
+
+
+
+} /* namespace one_wire_driver */
---
+++ OneWireDriver.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace one_wire_driver {
+
+class OneWireDriver {
+
+};
+
+} /* namespace one_wire_driver */
