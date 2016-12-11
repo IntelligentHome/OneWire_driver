60c2321fe7d4089cbbdac40745d128a6c62a416f
Mariusz Sikora <msikora87@gmail.com>
2016-12-11 11:36:51 +0100

    Basic class for OneWire driver

--- OneWireDriver.cpp
+++ OneWireDriver.cpp
@@ -2,6 +2,30 @@
 
 namespace one_wire_driver {
 
+OneWireDriver::OneWireDriver(
+        gpio_driver::IGpio& gpio,
+        wait::IWait&        wait)
+    :
+        gpio_(gpio),
+        wait_(wait)
+{
+}
+
+uint8_t OneWireDriver::Reset(void) {
+    return 0;
+}
+
+void OneWireDriver::Send(uint8_t send_buff[], uint16_t size) {
+
+}
+
+void OneWireDriver::Get(uint8_t recv_buff[], uint16_t size) {
+
+}
+
+void OneWireDriver::SendAndGet(uint8_t send_buff[], uint8_t recv_buff[], uint16_t size) {
+
+}
 
 
 } /* namespace one_wire_driver */
--- OneWireDriver.h
+++ OneWireDriver.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include "ITransport.h"
+#include "IGpioDriver.h"
+#include "IWait.h"
+
 namespace one_wire_driver {
 
-class OneWireDriver {
+class OneWireDriver : public transport::ITransport {
+
+public:
+
+    OneWireDriver(
+            gpio_driver::IGpio& gpio,
+            wait::IWait&        wait);
+
+    virtual uint8_t Reset(void);
+    virtual void Send(uint8_t send_buff[], uint16_t size);
+    virtual void Get(uint8_t recv_buff[], uint16_t size);
+    virtual void SendAndGet(uint8_t send_buff[], uint8_t recv_buff[], uint16_t size);
+
+private:
+    gpio_driver::IGpio& gpio_;
+    wait::IWait&        wait_;
 
 };
 
